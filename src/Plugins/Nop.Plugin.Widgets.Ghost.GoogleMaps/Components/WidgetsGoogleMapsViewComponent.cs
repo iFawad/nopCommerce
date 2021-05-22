2b1f8843097feec6c394622791e8e316b1ec2804
Fawad A. Khan <fawad1990@yahoo.com>
2021-05-22 23:36:39 +0500

    Added ViewComponent for Google Maps widget.

---
+++ src/Plugins/Nop.Plugin.Widgets.Ghost.GoogleMaps/Components/WidgetsGoogleMapsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.Ghost.GoogleMaps.Components
+{
+    [ViewComponent(Name = "WidgetsGoogleMaps")]
+    public class WidgetsGoogleMapsViewComponent : NopViewComponent
+    {
+        private readonly GoogleMapsSettings _googleMapsSettings;
+
+        public WidgetsGoogleMapsViewComponent(GoogleMapsSettings googleMapsSettings)
+        {
+            _googleMapsSettings = googleMapsSettings;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+        {
+            return View("~/Plugins/Widgets.Ghost.GoogleMaps/Views/GoogleMapsWidget.cshtml", _googleMapsSettings.MapEmbedScript);
+        }
+    }
+}
