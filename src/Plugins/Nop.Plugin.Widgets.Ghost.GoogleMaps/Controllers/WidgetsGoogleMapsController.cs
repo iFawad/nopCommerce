356c6c59107551ed1c09b6222c84e9b816050553
Fawad A. Khan <fawad1990@yahoo.com>
2021-05-22 22:59:31 +0500

    Added Configuration settings for Google Maps widget. Need to correct configuration label.

---
+++ src/Plugins/Nop.Plugin.Widgets.Ghost.GoogleMaps/Controllers/WidgetsGoogleMapsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Plugin.Widgets.Ghost.GoogleMaps.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Messages;
+using Nop.Services.Security;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.Widgets.Ghost.GoogleMaps.Controllers
+{
+    [Area(AreaNames.Admin)]
+    [AuthorizeAdmin]
+    [AutoValidateAntiforgeryToken]
+    public class WidgetsGoogleMapsController : BasePluginController
+    {
+        #region Fields
+
+        private readonly ILocalizationService _localizationService;
+        private readonly INotificationService _notificationService;
+        private readonly IPermissionService _permissionService;
+        private readonly ISettingService _settingService;
+        private readonly IStoreContext _storeContext;
+
+        #endregion
+
+        #region Ctor
+
+        public WidgetsGoogleMapsController(ILocalizationService localizationService,
+            INotificationService notificationService,
+            IPermissionService permissionService,
+            ISettingService settingService,
+            IStoreContext storeContext)
+        {
+            _localizationService = localizationService;
+            _notificationService = notificationService;
+            _permissionService = permissionService;
+            _settingService = settingService;
+            _storeContext = storeContext;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<IActionResult> Configure()
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManageWidgets))
+                return AccessDeniedView();
+
+            //load settings for a chosen store scope
+            var storeScope = await _storeContext.GetActiveStoreScopeConfigurationAsync();
+            var googleMapsSettings = await _settingService.LoadSettingAsync<GoogleMapsSettings>(storeScope);
+
+            var model = new ConfigurationModel
+            {
+                MapEmbedScript = googleMapsSettings.MapEmbedScript,
+                ActiveStoreScopeConfiguration = storeScope
+            };
+
+            if (storeScope > 0)
+            {
+                model.MapEmbedScript_OverrideForStore = await _settingService.SettingExistsAsync(googleMapsSettings, x => x.MapEmbedScript, storeScope);
+            }
+
+            return View("~/Plugins/Widgets.Ghost.GoogleMaps/Views/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Configure(ConfigurationModel model)
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManageWidgets))
+                return AccessDeniedView();
+
+            if (!ModelState.IsValid)
+                return await Configure();
+
+            //load settings for a chosen store scope
+            var storeScope = await _storeContext.GetActiveStoreScopeConfigurationAsync();
+            var cashOnDeliveryPaymentSettings = await _settingService.LoadSettingAsync<GoogleMapsSettings>(storeScope);
+
+            //save settings
+            cashOnDeliveryPaymentSettings.MapEmbedScript = model.MapEmbedScript;
+
+            /* We do not clear cache after each setting update.
+             * This behavior can increase performance because cached settings will not be cleared 
+             * and loaded from database after each update */
+            await _settingService.SaveSettingOverridablePerStoreAsync(cashOnDeliveryPaymentSettings, x => x.MapEmbedScript, model.MapEmbedScript_OverrideForStore, storeScope, false);
+
+            //now clear settings cache
+            await _settingService.ClearCacheAsync();
+
+            _notificationService.SuccessNotification(await _localizationService.GetResourceAsync("Admin.Plugins.Saved"));
+
+            return await Configure();
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.Ghost.GoogleMaps/GoogleMapsPluginProcessor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Core.Domain.Seo;
+using Nop.Services.Cms;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.Ghost.GoogleMaps
+{
+    public class GoogleMapsPluginProcessor : BasePlugin, IWidgetPlugin
+    {
+        private readonly ISettingService _settingService;
+        private readonly IWebHelper _webHelper;
+        private readonly ILocalizationService _localizationService;
+        private readonly IStoreContext _storeContext;
+        private readonly SeoSettings _seoSettings;
+
+        public GoogleMapsPluginProcessor(ISettingService settingService,
+            IWebHelper webHelper,
+            ILocalizationService localizationService,
+            IStoreContext storeContext,
+            SeoSettings seoSettings
+            )
+        {
+            _settingService = settingService;
+            _webHelper = webHelper;
+            _localizationService = localizationService;
+            _storeContext = storeContext;
+            _seoSettings = seoSettings;
+        }
+
+        public bool HideInWidgetList => false;
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "WidgetsGoogleMaps";
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> 
+            { PublicWidgetZones.ContactUsBottom }
+            );
+        }
+
+        /// <summary>
+        /// Gets a configuration page URL
+        /// </summary>
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/WidgetsGoogleMaps/Configure";
+        }
+
+        /// <summary>
+        /// Install plugin
+        /// </summary>
+        public override async Task InstallAsync()
+        {
+            // Adding Meta Tags.
+
+            var customHeadTags = _seoSettings.CustomHeadTags;
+            var finalCustomHeadTags = customHeadTags + "<meta name=\"referrer\"content=\"no-referrer-when-downgrade\">";
+            _seoSettings.CustomHeadTags = finalCustomHeadTags;
+
+            await _settingService.SaveSettingAsync(_seoSettings, x => x.CustomHeadTags);
+            await _settingService.ClearCacheAsync();
+
+
+            await _localizationService.AddLocaleResourceAsync(new Dictionary<string, string>
+            {
+                ["Plugins.Widgets.BsLiveChat.MapEmbedScript"] = "Google Maps Embed HTML code:",
+                ["Plugins.Widgets.BsLiveChat.MapEmbedScript.Hint"] = "Paste the html code generated from Google Maps Embed option."
+            });
+
+
+            await base.InstallAsync();
+        }
+
+        /// <summary>
+        /// Uninstall plugin
+        /// </summary>
+        public override async Task UninstallAsync()
+        {
+            // Deleting Meta Tags
+
+
+            //settings
+            await _settingService.DeleteSettingAsync<GoogleMapsSettings>();
+
+            //locales
+
+            await _localizationService.DeleteLocaleResourcesAsync("Plugins.Widgets.Ghost.GoogleMaps");
+            await base.UninstallAsync();
+        }
+    }
+}
