887e83b368748d552adfa8ffd5a2158edee34e99
Fawad A. Khan <fawad1990@yahoo.com>
2021-05-22 20:00:05 +0500

    Added displayPopupContentFromUrlYesNoAgeVerification js function.

---
+++ src/Presentation/Nop.Web/wwwroot/js/public.common.extension.js
@@ -0,0 +1,37 @@
+﻿/*
+** extension to custom js functions
+*/
+
+function displayPopupContentFromUrlYesNoAgeVerification(url, title, modal, width) {
+  var isModal = (modal ? true : false);
+  var targetWidth = (width ? width : 550);
+  var maxHeight = $(window).height() - 20;
+
+  $('<div></div>').load(url)
+    .dialog({
+      modal: isModal,
+      width: targetWidth,
+      maxHeight: maxHeight,
+      title: title,
+      buttons: [{
+        text: "Yes, I'm 21+",
+        click: function (event, ui) {
+          sessionStorage.setItem('advertOnce', 'true');
+          $(this).dialog('destroy').remove();
+        }
+      },
+      {
+        text: "No, I'm under 21",
+        click: function (event, ui) {
+          window.location.href = 'http://google.com/';
+          $(this).dialog('destroy').remove();
+        }
+      }],
+      open: function () {                      // open event handler
+        $(this)                               // the element being dialogged
+          .parent()                          // get the dialog widget element
+          .find(".ui-dialog-titlebar-close")
+          .hide();
+      }
+    });
+}
