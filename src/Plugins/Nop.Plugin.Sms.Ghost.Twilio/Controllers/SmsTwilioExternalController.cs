1dea617f636792d2d65a894c5b098150d4ee8022
Fawad A. Khan <fawad1990@yahoo.com>
2021-06-15 09:51:18 +0500

    Added SmsTwilioExternalController.

---
+++ src/Plugins/Nop.Plugin.Sms.Ghost.Twilio/Controllers/SmsTwilioExternalController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Plugin.Sms.Ghost.Twilio.Services;
+using Nop.Services.Configuration;
+using Nop.Services.Messages;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Sms.Ghost.Twilio.Controllers
+{
+    public class SmsTwilioExternalController : BasePluginController
+    {
+        #region Fields
+
+        private readonly INotificationService _notificationService;
+        private readonly ISettingService _settingService;
+        private readonly IStoreContext _storeContext;
+        private readonly TwilioSmsManager _twilioSmsManager;
+
+        #endregion
+
+        #region Ctor
+
+        public SmsTwilioExternalController(INotificationService notificationService,
+            ISettingService settingService,
+            IStoreContext storeContext,
+            TwilioSmsManager twilioSmsManager)
+        {
+            _notificationService = notificationService;
+            _settingService = settingService;
+            _storeContext = storeContext;
+            _twilioSmsManager = twilioSmsManager;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpPost]
+        public async Task Notify()
+        {
+            //try to send SMS
+            await _twilioSmsManager.SendSMSAsync("+12346578", "Chat started on: Friends Smoke & Vapor.");
+        }
+
+        #endregion
+    }
+}
