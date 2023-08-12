60ef74f9fde9de9c53f08175287567ff276efb1a
Fawad A. Khan <fawad1990@yahoo.com>
2023-08-12 21:05:47 +0400

    Added Google maps widget

---
+++ src/Plugins/Nop.Plugin.Widgets.Ghost.GoogleMaps/GoogleMapsSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Widgets.Ghost.GoogleMaps
+{
+    public class GoogleMapsSettings : ISettings
+    {
+        public string MapEmbedScript { get; set; }
+    }
+}
