5b03d5eb158ee60a8e5afbf26c5c7dd51748171b
Fawad A. Khan <fawad1990@yahoo.com>
2024-09-03 09:34:04 +0400

    Added plugin DiscountRules HadSpentAmount.

---
+++ src/Plugins/Nop.Plugin.DiscountRules.HadSpentAmount/Models/RequirementModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.DiscountRules.HadSpentAmount.Models;
+
+public record RequirementModel
+{
+    public int DiscountId { get; set; }
+
+    public int RequirementId { get; set; }
+
+    [NopResourceDisplayName("Plugins.DiscountRules.HadSpentAmount.Fields.Amount")]
+    public decimal SpentAmount { get; set; }
+}
